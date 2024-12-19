642bfaa88c855f3bb73dc2c8eee56237f0ec725e
dudemelo <1172271+dudemelo@users.noreply.github.com>
2024-12-19 07:20:11 +0100

    Remove the format on save

--- nvim/lua/plugins/autoformat.lua
+++ nvim/lua/plugins/autoformat.lua
@@ -14,14 +14,6 @@ return {
     },
     opts = {
         notify_on_error = false,
-        format_on_save = function(bufnr)
-            local disable_filetypes = { c = true, cpp = true }
-            return {
-                timeout_ms = 500,
-                lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
-                -- lsp_format = "fallback",
-            }
-        end,
         formatters_by_ft = {
             c = { "clang_format" },
             cpp = { "clang_format" },
