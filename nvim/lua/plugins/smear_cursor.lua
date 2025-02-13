285414ae868b90a7a25d12fe0a53aa9f78469c5d
dudemelo <1172271+dudemelo@users.noreply.github.com>
2025-02-13 10:53:12 +0100

    Use default configuration for the smear cursor

--- nvim/lua/plugins/smear_cursor.lua
+++ nvim/lua/plugins/smear_cursor.lua
@@ -1,9 +1,8 @@
 return {
     "sphamba/smear-cursor.nvim",
-    opts = { -- Default  Range
-        -- stiffness = 0.8, -- 0.6      [0, 1]
-        -- trailing_stiffness = 0.5, -- 0.3      [0, 1]
-        -- distance_stop_animating = 0.5, -- 0.1      > 0
-        -- hide_target_hack = false, -- true     boolean
+    version =  'v0.4.2',
+    opts = {
+        cursor_color = '#ffffff',
+        normal_bg = '#ffffff',
     },
 }
