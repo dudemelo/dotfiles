56f136799f98618f15552c789ab2da4f49e73c4e
dudemelo <1172271+dudemelo@users.noreply.github.com>
2025-02-13 10:52:08 +0100

    Disable the cursorhold to highlihght symbols usage

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -28,17 +28,17 @@ return {
 
                 local client = vim.lsp.get_client_by_id(event.data.client_id)
                 if client and client.server_capabilities.documentHighlightProvider then
-                    local highlight_augroup = vim.api.nvim_create_augroup("lsp-highlight", { clear = true })
-                    vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
-                        buffer = event.buf,
-                        group = highlight_augroup,
-                        callback = vim.lsp.buf.document_highlight,
-                    })
-                    vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
-                        buffer = event.buf,
-                        group = highlight_augroup,
-                        callback = vim.lsp.buf.clear_references,
-                    })
+                    -- local highlight_augroup = vim.api.nvim_create_augroup("lsp-highlight", { clear = true })
+                    -- vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+                    --     buffer = event.buf,
+                    --     group = highlight_augroup,
+                    --     callback = vim.lsp.buf.document_highlight,
+                    -- })
+                    -- vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
+                    --     buffer = event.buf,
+                    --     group = highlight_augroup,
+                    --     callback = vim.lsp.buf.clear_references,
+                    -- })
                     vim.api.nvim_create_autocmd("LspDetach", {
                         group = vim.api.nvim_create_augroup("lsp-detach", { clear = true }),
                         callback = function(event2)
