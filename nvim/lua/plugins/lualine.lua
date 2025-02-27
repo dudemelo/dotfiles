17599442d9d9b401950569efff9513d1bcff812b
dudemelo <1172271+dudemelo@users.noreply.github.com>
2025-02-27 06:00:09 +0100

    Disable lualine icons

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -2,7 +2,7 @@ return {
 	"nvim-lualine/lualine.nvim",
 	opts = {
 		options = {
-			icons_enabled = true,
+			icons_enabled = false,
 			theme = "iceberg_dark",
 			component_separators = { left = "|", right = "|" },
 			section_separators = { left = nil, right = nil },
