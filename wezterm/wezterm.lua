40a93920dc33ee88e5599566270dbc6bd1ad192a
dudemelo <1172271+dudemelo@users.noreply.github.com>
2025-08-31 11:23:46 +0200

    Install formatter and update fps for wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 config.adjust_window_size_when_changing_font_size = false
-config.animation_fps = 120
+config.animation_fps = 60
 config.check_for_updates = false
 
 -- config.color_scheme = "catppuccin-mocha"
