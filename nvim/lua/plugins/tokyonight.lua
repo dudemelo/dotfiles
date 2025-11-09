7f117eb432f9d0637c89d8e27f789a9ff518678b
dudemelo <1172271+dudemelo@users.noreply.github.com>
2025-11-09 04:21:52 +0100

    Ovewrite bg color of tokyo night

--- nvim/lua/plugins/tokyonight.lua
+++ nvim/lua/plugins/tokyonight.lua
@@ -7,6 +7,9 @@ return {
 			styles = {
 				keywords = { bold = true },
 			},
+			on_colors = function(colors)
+				colors.bg = "#000000"
+			end,
 		})
 		vim.cmd.colorscheme("tokyonight-night")
 	end,
