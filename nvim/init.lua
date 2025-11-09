c5c6a86a717d7a806d400bbeb0ab8f3b1a58ec74
dudemelo <1172271+dudemelo@users.noreply.github.com>
2025-11-09 12:11:11 +0100

    Add shortcut to return to the file explorer

--- nvim/init.lua
+++ nvim/init.lua
@@ -33,6 +33,9 @@ vim.o.wrap = false -- don't wrap based on the window size
 vim.o.hlsearch = true
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
+-- shortcut to return to the file explorer
+vim.keymap.set("n", "<leader>ex", "<Cmd>Explore<CR>")
+
 -- move lines up and down in visual mode
 -- vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Moves line down" })
 -- vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Moves line up" })
