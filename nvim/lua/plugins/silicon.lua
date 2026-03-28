5a5aa63c1b6ba465bbe599160aa11a713ea69090
dudemelo <1172271+dudemelo@users.noreply.github.com>
2026-03-28 16:20:31 +0100

    Add silicon to generate screenshot

---
+++ nvim/lua/plugins/silicon.lua
@@ -0,0 +1,15 @@
+return {
+	"michaelrommel/nvim-silicon",
+	lazy = true,
+	cmd = "Silicon",
+	main = "nvim-silicon", -- important for newer versions
+	opts = {
+		font = "IosevkaTermNerdFontMono",
+		theme = "Dracula",
+		no_window_controls = true,
+		no_line_number = true,
+		background = "#9d4edd",
+		shadow_color = "#9d4edd",
+		to_clipboard = true,
+	},
+}
